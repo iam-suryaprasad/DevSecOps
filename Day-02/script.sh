fd0e1d0bc1b28495d87b945882555f12a0f9c5ba
iam-suryaprasad <msuryaprasad11@gmail.com>
2024-09-26 21:43:40 +0530

    Day-03 shell scripting notes

--- Day-02/script.sh
+++ Day-02/script.sh
@@ -1,16 +1,29 @@
-#!bin/bash
-if [ $# -gt 0 ]
-then 
-aws --version
-if [ $? -eq 0]; then
-REGIONS = $@
-for REGION in ${REGIONS}; do
-    aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r
-    echo '________________________'
-  done
+#!/bin/bash
+
+if [ $# -gt 0 ]; then
+    aws --version
+    if [ $? -eq 0 ]; then
+        REGIONS="$@"
+        for REGION in ${REGIONS}; do
+            echo "VPCs in region: $REGION"
+            aws ec2 describe-vpcs --region "$REGION" | jq ".Vpcs[].VpcId" -r
+            echo '________________________'
+        done
+    else
+        echo "Incorrect command."
+    fi
 else
-     echo " incorrect command "
+    echo "Please enter the arguments."
 fi
-else
-echo " please enter the arguments "
-fi
+
+
+
+
+#!/bin/bash
+echo "Welcome DevSecOps day-02"
+for arg in "$*"; do
+    echo "$arg"
+done
+
+
+
